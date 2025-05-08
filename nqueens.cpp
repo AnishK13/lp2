9c86d3314978a7220303db5096cb86f30433fe76
AnishK13 <anishkhachane2004@gmail.com>
2025-05-08 20:05:04 +0530

    nqueens assignment added

---
+++ nqueens.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(int col, int n, vector<string> &board, vector<vector<string>> &ans, 
+  vector<int> &leftrow, vector<int> &upperdiag, vector<int> &lowerdiag){
+    if(col==n){
+      ans.push_back(board);
+      return;
+    }
+
+    for(int row=0; row<n; row++){
+      if(leftrow[row]==0 && upperdiag[row-col + n-1]==0 && lowerdiag[row+col]==0){
+        board[row][col]='Q';
+        leftrow[row]=1;
+        upperdiag[row-col + n-1]=1;
+        lowerdiag[row+col]=1;
+        solve(col+1, n, board, ans, leftrow, upperdiag, lowerdiag);
+        board[row][col]='.';
+        leftrow[row]=0;
+        upperdiag[row-col + n-1]=0;
+        lowerdiag[row+col]=0;
+      }
+    }
+}
+
+
+int main(){
+  int choice=1;
+  while(choice==1){
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    vector<vector<string>>ans;
+    string s(n,'.');
+    vector<string>board(n,s);
+    vector<int> leftrow(n,0), upperdiag(2*n-1,0), lowerdiag(2*n-1,0);
+    solve(0,n,board,ans,leftrow,upperdiag,lowerdiag);
+    cout<<"ans size: "<<ans.size()<<endl;
+    for(auto i: ans){
+      for(auto j: i){
+        cout<<j<<endl;
+      }
+      cout<<endl;
+    }
+    cout<<"Enter choice(0/1): ";
+    cin>>choice;
+  }
+  
+}
